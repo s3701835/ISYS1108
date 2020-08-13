85e1f5c6274b8419fce73c5db64331762009aeb1
Jason Chandra <s3730387@student.rmit.edu.au>
2020-08-13 17:53:33 +1000

    Create edit-location.php

---
+++ edit-location.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <title>Edit Location | Tour Management</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- JavaScript from Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <!-- CSS from Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+
+    <!-- Individualised CSS -->
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- Edit Location Field -->
+
+    <!-- Edit Location Field -->
+
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
